59f5c8a10a0f6d3ead6306203d5cd5f2b492cc61
shubham kulthe <kulthe152@gmail.com>
2021-05-11 18:57:08 +0530

    add orgnisation project to Localrepo

---
+++ src/main/java/net/org/dto/Employee.java
@@ -0,0 +1,38 @@
+package net.org.dto;
+
+public class Employee {
+	
+	private long eid;
+	private String empName;
+	private double empSal;
+	private String deptName;
+	
+	public long getEid() {
+		return eid;
+	}
+	public void setEid(long eid) {
+		this.eid = eid;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	
+	
+}
+
